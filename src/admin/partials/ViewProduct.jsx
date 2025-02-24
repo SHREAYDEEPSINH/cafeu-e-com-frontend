8b09e1a7d74ecd8f5d65d654949fe27b3b90c473
SHREAYDEEPSINH <shreaydeepsinhvaghela@gmail.com>
2025-02-24 14:41:31 +0530

    cafeu-frontend

--- src/admin/partials/ViewProduct.jsx
+++ src/admin/partials/ViewProduct.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 
 
 function ViewProduct() {
-    const navigate = useNavigate();
     const [allProducts, setAllProducts] = useState([])
 
     useEffect(() => {
