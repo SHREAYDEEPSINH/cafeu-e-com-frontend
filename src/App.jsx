8bdb7387c56ab8122cfc511ab548682fd3ccb820
SHREAYDEEPSINH <shreaydeepsinhvaghela@gmail.com>
2025-03-01 22:26:02 +0530

    cafeu-frontend

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React, { useEffect } from 'react';
 import {
   Routes,
   Route,
+  useLocation
 } from 'react-router-dom';
 
 import './admin/css/style.css';
 
+import './admin/charts/ChartjsConfig';
 
 // Import pages
 import Dashboard from './admin/pages/Dashboard';
@@ -17,13 +19,13 @@ import ShowProduct from './admin/pages/ShowProduct';
 
 function App() {
 
-  // const location = useLocation();
+  const location = useLocation();
 
-  // useEffect(() => {
-  //   document.querySelector('html').style.scrollBehavior = 'auto'
-  //   window.scroll({ top: 0 })
-  //   document.querySelector('html').style.scrollBehavior = ''
-  // }, [location.pathname]); // triggered on route change
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+  }, [location.pathname]); // triggered on route change
 
   let adminPanel=()=>{
     return <Dashboard/>
